f68357751b9d8b7f0d0b52337c76f201265a44e8
Lemonade-aaa <julianr4258@masseyhigh.school.nz>
2026-02-09 09:25:08 +1300

    Looping yes/no checker

--- 01_Roll_It_13/C_01_yes_no_basic.py
+++ 01_Roll_It_13/C_01_yes_no_basic.py
@@ -1,7 +1,16 @@
-want_instructions =input("Would you like to see the instructions?").lower()
-if want_instructions == "yes" or want_instructions == "y":
-    print("You have responded with yes.")
-elif want_instructions == "no" or want_instructions == "n":
-    print("You have responded with no.")
-else:
-    print("Answer yes or no..")
+
+
+while True:
+    want_instructions = input("Would you like to see the instructions?").lower()
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You have responded with yes.")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You have responded with no.")
+        break
+    else:
+        print("Answer yes or no..")
+        continue
+
+print("We have completed the steps prior and are bring this to a close")
